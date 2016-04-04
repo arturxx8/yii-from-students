91b6d7d096bde02eb87d36c40a70cc6354e93f37
arturxx8 <blik373@gmail.com>
2016-04-04 15:09:41 +0200

    Update _order.php

--- views/orders/_order.php
+++ views/orders/_order.php
@@ -1,12 +1,8 @@
 <?php
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
-use yii\widgets\Pjax;
 
-$form = ActiveForm::begin(['id'=>'wo13','options'=>['data-pjax' => true]]); ?>
-
-<?= $form->field($model, 'order_id')->textInput(['disabled'=>true]) ?>
-<?= $form->field($model, 'order_id')->hiddenInput()->label(false) ?>
+$form = ActiveForm::begin(['id'=>'wo13']); ?>
 <?= $form->field($model, 'department_id')->dropDownList($deps) ?>
 <?= $form->field($model, 'comment')->textarea(['rows' => 6, 'required'=>"required"]) ?>
 
